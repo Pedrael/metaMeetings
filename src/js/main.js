82553ef86dbc8c6f24f1665f3cb35e132571feed
Mihail <pedrael@pop-os.localdomain>
2020-09-23 01:23:22 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,19 @@ function toggleAccordeon() {
   items.forEach((item) => {
     item.addEventListener('click', (e) => {
       items.forEach(i => {
-        if(i != e.currentTarget) i.classList.remove('selected')
+        if(i != e.currentTarget) {
+          i.classList.remove('selected')
+          var panel = i.querySelector('.panel')
+          panel.style.maxHeight = null;
+        }
       })
       e.currentTarget.classList.toggle('selected')
+      var panel = e.currentTarget.querySelector('.panel')
+       if (panel.style.maxHeight) {
+         panel.style.maxHeight = null;
+       } else {
+         panel.style.maxHeight = panel.scrollHeight + "px";
+       }
     })
   })
 }
