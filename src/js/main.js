27535447be397f6610d7514a65b1dda4aea0adb4
Mihail <pedrael@pop-os.localdomain>
2020-09-24 15:43:08 +0300

    added calculator

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,38 @@ function toggleAccordeon() {
   })
 }
 
+function smoothCalc(oldV, newV, link) {
+  console.log(oldV, newV)
+  if(oldV == newV) return;
+  let dif = Math.abs(oldV - newV)
+  let iii = setInterval(() => {
+    oldV > newV ? document.querySelector(link).innerHTML-- : document.querySelector(link).innerHTML++
+    document.querySelector(link).innerHTML == newV ? clearInterval(iii) : null
+  }, 30)
+
+}
+
+function calculateValues() {
+  let input = document.querySelector('#jscalcinput')
+  let v = Number.parseInt(input.value)
+  input.oninput = function() {
+    if(input.value < 1) input.value = 1
+    if(input.value > 12) input.value = 12
+    v = Number.parseInt(input.value)
+  }
+
+  let button = document.querySelector('#jscalcbtn')
+  button.onclick = function(e) {
+    e.preventDefault()
+    smoothCalc(document.querySelector('#jscalcv1').innerHTML, v * 4, '#jscalcv1')
+    smoothCalc(document.querySelector('#jscalcv2').innerHTML, v * 2, '#jscalcv2')
+    smoothCalc(document.querySelector('#jscalcv3').innerHTML, v * 2, '#jscalcv3')
+  }
+
+}
+
 toggleAccordeon()
+calculateValues()
 
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 900,
